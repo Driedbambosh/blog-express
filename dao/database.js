ad15f05e5209bbfc916da5adf0478d0b6a032b5f
Driedbambosh <1145761792@qq.com>
2021-12-20 20:23:51 +0800

    update

--- dao/database.js
+++ dao/database.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-14 21:44:05
+ * @LastEditTime: 2021-12-17 22:54:20
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \blog-express\dao\database.js
+ */
 // mongoDb
 const mongoose = require('mongoose');
 // 数据库地址
 // const dbURL = 'mongodb://192.168.0.117:27017/expressTest'
-const dbURL = 'mongodb://192.168.1.15:27017/expressTest'
+const dbURL = 'mongodb://localhost:27017/expressTest'
 mongoose.connect(dbURL);
 // 数据库连接成功
 mongoose.connection.on('connected', () => {
