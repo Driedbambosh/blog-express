6fdbd4668023f9ff0747c5b240870208343faa77
Driedbambosh <1145761792@qq.com>
2022-08-06 19:18:46 +0800

    update

--- dao/database.js
+++ dao/database.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Driedbambosh 1145761792@qq.com
+ * @Date: 2022-08-06 19:18:01
+ * @LastEditors: Driedbambosh 1145761792@qq.com
+ * @LastEditTime: 2022-08-06 19:18:32
+ * @FilePath: \blog-express\dao\database.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 // mongoDb
 const mongoose = require('mongoose');
 // 数据库地址
