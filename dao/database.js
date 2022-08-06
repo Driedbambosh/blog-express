ebbf8e06c196ad3b2fd133886a24eac5ddb39ebc
Driedbambosh <1145761792@qq.com>
2022-08-06 19:22:29 +0800

    update

--- dao/database.js
+++ dao/database.js
@@ -2,7 +2,7 @@
  * @Author: Driedbambosh 1145761792@qq.com
  * @Date: 2022-08-06 19:18:01
  * @LastEditors: Driedbambosh 1145761792@qq.com
- * @LastEditTime: 2022-08-06 19:18:32
+ * @LastEditTime: 2022-08-06 19:22:03
  * @FilePath: \blog-express\dao\database.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
